834b947e8d25682978eb2c779a5653bee2058736
Fethi E <jack@jacks-MacBook-Pro.local>
2019-04-23 16:50:28 -0500

    last initial

--- closures.js
+++ closures.js
@@ -23,12 +23,15 @@ function outer() {
 */
   
 // Code Here
+var inner = outer()
+
 
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+inner()
 
 
 
@@ -52,8 +55,9 @@ function callFriend(name) {
 */
 
 //Code Here
+let callJake = callFriend("Jake")
 
-
+callJake('435-555-9248')
 
 ////////// PROBLEM 3 //////////
 
@@ -62,15 +66,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  var num = 1;
+    function inner(){
+      return num++;
+    }
+  return inner;
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +97,23 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  value;
   return {
-
+    inc: function(){
+      return value+=1;
+      
+    },
+    dec: function(){
+      return value-=1;
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +129,13 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message(){
+    return `You're doing awesome, keep it up ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  //return message
+  return message
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,9 +164,13 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
-  };
+    publicMethod: function(){
+    return privateMethod();
+    }
+  }
 })();
 
+module.publicMethod();
 
 
 ////////// PROBLEM 7 //////////
@@ -163,6 +187,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num){
+    return secret += num
+    },
+    takeAwayFromSecret: function(num){
+      return secret -= num
+    }
   };
 }
 
